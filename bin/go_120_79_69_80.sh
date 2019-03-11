f3d720dc518162531cb7265f1c1f64c0e2d4f128
小嗷嗷 <xiaoaoao@192.168.0.15>
2019-03-11 09:44:03 +0800

    delete passwd from code

--- bin/go_120_79_69_80.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect -f
-set time 30
-spawn ssh root@120.79.69.80
-expect {
-"*yes/no" { send "yes\r"; exp_continue }
-"*password:" { send "Www17tao@123\r" }
-}
-interact
--- bin/go_39_108_213_67.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect -f
-set time 30
-spawn ssh root@39.108.213.67
-expect {
-"*yes/no" { send "yes\r"; exp_continue }
-"*password:" { send "Www17tao@123\r" }
-}
-interact
